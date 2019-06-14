bce787ea2972b87320314a9506b98235a3c53343
Katlin <katlin.nbanderson@gmail.com>
2019-06-14 15:53:48 -0700

    add hover effects to carosel video

--- src/app/carosel/carosel.component.ts
+++ src/app/carosel/carosel.component.ts
@@ -9,7 +9,7 @@ import { Video } from '../video';
 export class CaroselComponent implements OnInit {
   
   constructor() { }
-  // videos = [new Video('Catmare Before Christmas', '#', 'Cat Skellington ruins tha family Christmas for a third year by peeing in everyones stocking'), new Video('Feline and Furious 43', '#', 'Two cats go head to head in a face to the food bowl'), new Video('Catasblanca', '#', 'Loner cat, Rick, just wants to be left alone, but an old flame, Fluffy, is about to walk in front of his window again'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')];
+  videos = [new Video('Catmare Before Christmas', '#', 'Cat Skellington ruins tha family Christmas for a third year by peeing in everyones stocking'), new Video('Feline and Furious 43', '#', 'Two cats go head to head in a face to the food bowl'), new Video('Catasblanca', '#', 'Loner cat, Rick, just wants to be left alone, but an old flame, Fluffy, is about to walk in front of his window again'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')];
 
   ngOnInit() {
 
