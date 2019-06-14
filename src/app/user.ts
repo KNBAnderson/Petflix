aca0c92ad04b7bebb22c54a59dfc5eaf0dd73037
Katlin <katlin.nbanderson@gmail.com>
2019-06-14 12:02:47 -0700

    add User class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    constructor(public name: string, public avatar: string) {
+    }
+
+    editUser(newName: string, newAvatar: string) {
+        if(newName) {
+            this.name = newName;
+        }
+        if(newAvatar) {
+            this.avatar = newAvatar;
+        }
+    }
 }
