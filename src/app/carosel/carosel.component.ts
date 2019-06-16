842450961d1d4807586e9b52b1aab01de61a7714
Katlin <katlin.nbanderson@gmail.com>
2019-06-15 23:47:30 -0700

    refactored carosel component to be more reusable

--- src/app/carosel/carosel.component.ts
+++ src/app/carosel/carosel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Video } from '../video';
+import { createList } from '../video';
 
 @Component({
   selector: 'app-carosel',
@@ -9,8 +9,8 @@ import { Video } from '../video';
 export class CaroselComponent implements OnInit {
   
   constructor() { }
-  catVideos = [new Video('Catmare Before Christmas', '#', 'Cat Skellington ruins tha family Christmas for a third year by peeing in everyones stocking'), new Video('Feline and Furious 43', '#', 'Two cats go head to head in a face to the food bowl'), new Video('Catasblanca', '#', 'Loner cat, Rick, just wants to be left alone, but an old flame, Fluffy, is about to walk in front of his window again'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')];
-
+  // catVideos = [new Video('Catmare Before Christmas', '#', 'Cat Skellington ruins tha family Christmas for a third year by peeing in everyones stocking'), new Video('Feline and Furious 43', '#', 'Two cats go head to head in a race to the food bowl'), new Video('Catasblanca', '#', 'Loner cat, Rick, just wants to be left alone, but an old flame, Fluffy, is about to walk in front of his window again'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')];
+  objectArray = createList();
   ngOnInit() {
 
   }
--- src/app/video.ts
+++ src/app/video.ts
@@ -3,3 +3,24 @@ export class Video {
     
     }
 }
+
+export function createList() {
+        var videoList = new Array();
+        videoList.push({
+            title: 'Cat Capers',
+            category: 'cat', 
+            videos: [new Video('Catmare Before Christmas', '#', 'Cat Skellington ruins tha family Christmas for a third year by peeing in everyones stocking'), new Video('Feline and Furious 43', '#', 'Two cats go head to head in a race to the food bowl'), new Video('Catasblanca', '#', 'Loner cat, Rick, just wants to be left alone, but an old flame, Fluffy, is about to walk in front of his window again'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')]});
+        videoList.push({
+            title: 'Dog Dramas', 
+            category: 'dog',
+            videos: [new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')]});
+        videoList.push({
+            title: 'Canary Comedies', 
+            category: 'bird',
+            videos: [new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')]});
+        videoList.push({
+            title: 'Zombie Ferrets', 
+            category: 'ferrets',
+            videos: [new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')]});
+        return videoList;
+}
