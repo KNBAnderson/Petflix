7adf4b46e3393a2dbcb974158c4436f40c6d9c86
Katlin <katlin.nbanderson@gmail.com>
2019-06-21 12:20:25 -0700

    correct wrong changes and get mock-videos to display

--- src/app/carosel/carosel.component.ts
+++ src/app/carosel/carosel.component.ts
@@ -5,21 +5,20 @@ import { Router } from '@angular/router';
 import { User } from './../user';
 import { VideoService } from '../video.service';
 
-
 @Component({
   selector: 'app-carosel',
   templateUrl: './carosel.component.html',
   styleUrls: ['./carosel.component.css'],
   providers: [VideoService, UserService]
 })
+
 export class CaroselComponent implements OnInit {
   videos: Object[];
   
-  constructor(private router: Router, private videoService: VideoService, private userService: UserService) {}
-
+  constructor(private router: Router, private videoService: VideoService) {}
 
   ngOnInit() {
     this.videos = this.videoService.getVideos();
+    console.log(this.videos);
   }
-
 }
