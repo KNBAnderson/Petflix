a0cb9ad17246b8507e59c1bb0c532b7610304748
Katlin <katlin.nbanderson@gmail.com>
2019-06-21 14:44:46 -0700

    create sample file that firebase will not accept

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Video } from './video';
 import { VIDEOS } from './mock-videos';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
-
+  videos: FirebaseListObservable<any[]>;
   constructor() { }
 
   getVideos() {
