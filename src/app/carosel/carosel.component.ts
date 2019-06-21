8b72aeaca485e126ca711a853d558109bedbeb57
Katlin <katlin.nbanderson@gmail.com>
2019-06-21 08:18:20 -0700

    add styling changes and avatar images

--- src/app/carosel/carosel.component.ts
+++ src/app/carosel/carosel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { createList } from '../video';
+//import { VIDEOS } from '../mock-videos';
+import { Video } from './../video';
+
 
 @Component({
   selector: 'app-carosel',
@@ -9,8 +11,8 @@ import { createList } from '../video';
 export class CaroselComponent implements OnInit {
   
   constructor() { }
-  // catVideos = [new Video('Catmare Before Christmas', '#', 'Cat Skellington ruins tha family Christmas for a third year by peeing in everyones stocking'), new Video('Feline and Furious 43', '#', 'Two cats go head to head in a race to the food bowl'), new Video('Catasblanca', '#', 'Loner cat, Rick, just wants to be left alone, but an old flame, Fluffy, is about to walk in front of his window again'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj'), new Video('Lorem', '#', 'Lorem Ipsum dkfjdsjfkdljfkdjflkdsjflkdsjflkdsjfkj')];
-  objectArray = createList();
+
+  videos: Video[];
   ngOnInit() {
 
   }
